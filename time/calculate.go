0d22720cef15e6f4178181589146687b0aee7802
Zhang Peng <onnttf@gmail.com>
2025-04-07 20:17:24 +0800

    docs: improve documentation

--- time/calculate.go
+++ time/calculate.go
@@ -2,12 +2,12 @@ package time
 
 import "time"
 
-// StartOfDay returns the time corresponding to the start of the day (midnight) in the same time zone.
+// StartOfDay returns the start of the given day (00:00:00.000) in the same time zone.
 func StartOfDay(t time.Time) time.Time {
 	return time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, t.Location())
 }
 
-// EndOfDay returns the time corresponding to the end of the day (just before midnight) in the same time zone.
+// EndOfDay returns the end of the given day (23:59:59.999...) in the same time zone.
 func EndOfDay(t time.Time) time.Time {
 	return time.Date(t.Year(), t.Month(), t.Day(), 23, 59, 59, 999999999, t.Location())
 }
