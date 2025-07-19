8d66bc82214bdc0e16dac0f6b15d4e265770b06c
Zhang Peng <onnttf@gmail.com>
2025-07-19 12:54:06 +0800

    refactor: refactor code comments

--- time/calculate.go
+++ time/calculate.go
@@ -2,12 +2,12 @@ package time
 
 import "time"
 
-// StartOfDay returns the start of the given day (00:00:00.000) in the same time zone.
+// StartOfDay returns a timestamp set to the beginning of the given day (00:00:00.000)
 func StartOfDay(t time.Time) time.Time {
 	return time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, t.Location())
 }
 
-// EndOfDay returns the end of the given day (23:59:59.999...) in the same time zone.
+// EndOfDay returns a timestamp set to the end of the given day (23:59:59.999999999)
 func EndOfDay(t time.Time) time.Time {
 	return time.Date(t.Year(), t.Month(), t.Day(), 23, 59, 59, 999999999, t.Location())
 }
